2e380225e73df18af4109b60e7607a17b57d9674
rvjs <renato.valentim@tempest.com.br>
2023-07-14 23:45:15 -0300

    feat(against_example): start the server

--- cmd/invoice/main.go
+++ cmd/invoice/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "log"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 func main() {
-	log.Print(`Hello from main. by Renato`)
+	e := echo.New()
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{Format: "method=${method}, uri=${uri}, status=${status}\n"}))
+	e.Logger.Fatal(e.Start(`:3000`))
 }
