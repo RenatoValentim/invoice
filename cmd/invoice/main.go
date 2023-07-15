3c13c8195a3686139c163beb90ef0093b9d19595
rvjs <renato.valentim@tempest.com.br>
2023-07-14 23:54:01 -0300

    feat(against_example): add invoices route

--- cmd/invoice/main.go
+++ cmd/invoice/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -8,5 +10,8 @@ import (
 func main() {
 	e := echo.New()
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{Format: "method=${method}, uri=${uri}, status=${status}\n"}))
+	e.GET(`/cards/:cardNumber/invoices`, func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]int{`total`: 1000})
+	})
 	e.Logger.Fatal(e.Start(`:3000`))
 }
